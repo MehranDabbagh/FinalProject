e4146b33de2dc97e4a68b5645b9392bd68b8b4f2
ASUS <mehrandab78@gmail.com>
2022-05-25 12:28:39 +0430

    exception handling

---
+++ src/main/java/com/example/PhaseTwo/model/exception/GlobalExceptionHandling.java
@@ -0,0 +1,27 @@
+package com.example.PhaseTwo.model.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.InputMismatchException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandling {
+    @ExceptionHandler(value = NullPointerException.class)
+    public ResponseEntity<Object> nullPointHnadler(Exception ex, WebRequest request) {
+        return ResponseEntity.ok(null);
+    }
+
+    @ExceptionHandler(value = InputMismatchException.class)
+    public ResponseEntity<Object> inputMismatch(Exception ex, WebRequest request) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(value = RuntimeException.class)
+    public ResponseEntity<Object> allHandler(Exception ex, WebRequest request) {
+        return ResponseEntity.ok(null);
+    }
+}
