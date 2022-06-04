42ee190947d247f9b12ec87019ccd73773d8bc71
ASUS <mehrandab78@gmail.com>
2022-06-05 01:11:35 +0430

    editing comment saving process

--- src/main/java/com/example/PhaseTwo/model/service/impl/CommentServiceImpl.java
+++ src/main/java/com/example/PhaseTwo/model/service/impl/CommentServiceImpl.java
@@ -1,25 +1,35 @@
 package com.example.PhaseTwo.model.service.impl;
 
+import com.example.PhaseTwo.model.entity.Bid;
 import com.example.PhaseTwo.model.entity.Comment;
+import com.example.PhaseTwo.model.entity.Orders;
+import com.example.PhaseTwo.model.entity.Status;
+import com.example.PhaseTwo.model.repository.BidRepository;
 import com.example.PhaseTwo.model.repository.CommentRepository;
+import com.example.PhaseTwo.model.repository.OrdersRepository;
 import com.example.PhaseTwo.model.service.CommentService;
 import org.hibernate.sql.Update;
 import org.springframework.stereotype.Service;
 
+import java.util.InputMismatchException;
 import java.util.List;
 
 @Service
 public class CommentServiceImpl implements CommentService {
     private CommentRepository commentRepository;
+    private BidRepository bidRepository;
+    private OrdersRepository ordersRepository;
 
-    public CommentServiceImpl(CommentRepository commentRepository) {
+    public CommentServiceImpl(CommentRepository commentRepository, BidRepository bidRepository, OrdersRepository ordersRepository) {
         this.commentRepository = commentRepository;
+        this.bidRepository = bidRepository;
+        this.ordersRepository = ordersRepository;
     }
 
     @Override
     public Comment findById(Long id) {
-        Comment comment=commentRepository.findById(id).orElse(null);
-        if(comment!=null) {
+        Comment comment = commentRepository.findById(id).orElse(null);
+        if (comment != null) {
             return comment;
         }
         throw new NullPointerException("wrong id!");
@@ -27,11 +37,19 @@ public class CommentServiceImpl implements CommentService {
 
     @Override
     public Comment save(Comment comment) {
+        Bid bid = bidRepository.findById(comment.getBid().getId()).orElse(null);
+        if (bid == null) {
+            throw new InputMismatchException("bad request!");
+        }
+        Orders orders = ordersRepository.findById(bid.getOrders().getId()).orElse(null);
+        if (orders == null || orders.getStatus() != Status.Done) {
+            throw new InputMismatchException("bad request!");
+        }
         return commentRepository.save(comment);
     }
 
     @Override
-    public Comment update(Comment comment){
+    public Comment update(Comment comment) {
         return commentRepository.save(comment);
     }
 
